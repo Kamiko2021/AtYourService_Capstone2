d3490becc1d7d6f3a8f28fb44a23218d0c900620
Kamiko2021 <klopez_ccs@uspf.edu.ph>
2022-09-23 02:10:37 +0800

    initial fix of the layout, created the initial dashboard and profile.

--- app/src/main/java/com/capstone/atyourservice_capstone2/ClientProfileSettings.java
+++ app/src/main/java/com/capstone/atyourservice_capstone2/ClientProfileSettings.java
@@ -9,66 +9,28 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ClientProfileSettings#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ClientProfileSettings extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ClientProfileSettings() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ClientProfileSettings.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ClientProfileSettings newInstance(String param1, String param2) {
-        ClientProfileSettings fragment = new ClientProfileSettings();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    TextView emailtxt,fullnametxt;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
-    //==========Declarations========
-//    TextView emailtxt,fullnametxt,txtbirthdate;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_client_profile_settings, container, false);
+        View view= inflater.inflate(R.layout.fragment_client_profile_settings, container, false);
 
 
 
-        //=========Set up ID for declared objects====
-//        emailtxt=(TextView) getView().findViewById(R.id.email_lbl);
-//        fullnametxt=(TextView) getView().findViewById(R.id.fullname_lbl);
-
+       // =========Set up ID for declared objects====
+        emailtxt=(TextView) view.findViewById(R.id.email_lbl);
+        fullnametxt=(TextView) view.findViewById(R.id.fullname_lbl);
+        return view;
     }
 
     public void fetchData(){
